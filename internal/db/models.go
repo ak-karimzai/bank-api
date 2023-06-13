0aa535809d53f3b371c0699684244401008ea32e
Ahmad Khalid Karimzai <ak.karimzai1@mail.ru>
2023-06-13 23:15:40 +0300

    add dbdocs

--- internal/db/models.go
+++ internal/db/models.go
@@ -8,6 +8,8 @@ import (
 	"database/sql/driver"
 	"fmt"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type Currency string
@@ -67,6 +69,17 @@ type Entry struct {
 	// can be negative and positive
 	Amount    int64     `json:"amount"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+type Session struct {
+	ID           uuid.UUID `json:"id"`
+	Username     string    `json:"username"`
+	RefreshToken string    `json:"refresh_token"`
+	UserAgent    string    `json:"user_agent"`
+	ClientIp     string    `json:"client_ip"`
+	IsBlocked    bool      `json:"is_blocked"`
+	ExpiresAt    time.Time `json:"expires_at"`
+	CreatedAt    time.Time `json:"created_at"`
 }
 
 type Transfer struct {
--- makefile
+++ makefile
@@ -12,8 +12,16 @@ sqlc:
 test: 
 	go test -v -cover ./...
 
+db_docs:
+	dbdocs build doc/db.dbml
+
+db_schema:
+	dbml2sql --postgres -o doc/schema.sql doc/db.dbml
+
 mock:
 	mockgen --destination=internal/db/mock/store.go --package=mockdb github.com/ak-karimzai/ak-karimzai/simpleb/internal/db Store
 
 clean: 
 	rm -f server
+
+.PHONY: db_schema db_docs 
